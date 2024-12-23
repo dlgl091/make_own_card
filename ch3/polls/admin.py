f5a3a6e32bd85f5cf150763b51024558613eeef8
dlgl091 <82769484+dlgl091@users.noreply.github.com>
2024-12-24 00:26:06 +0900

    Delete ch3 directory

--- ch3/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from polls.models import TemplateCard, GeneratedCard
-
-# Register your models here.
-
-admin.site.register(TemplateCard)
-admin.site.register(GeneratedCard)

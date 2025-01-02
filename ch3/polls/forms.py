bfdefbd13b3c56a1ccacf4c3ed3748d5cb1225a2
dlgl091 <82769484+dlgl091@users.noreply.github.com>
2025-01-03 01:44:55 +0900

    Delete ch3 directory

--- ch3/polls/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class SubmitForm(forms.Form): # ModelForm 은 장고 모델 폼
-    cardNo = forms.IntegerField(primary_key=True)
-    category = forms.CharField(max_length=10)  # category
-    genImgLink = forms.ImageField(null=True, upload_to="", blank=True)  # genImgLink
-    text1 = forms.CharField(max_length=20, default="")
-    text2 = forms.CharField(max_length=20, default="")
